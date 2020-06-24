93d0436d9de4f6de6fc9f9938c4a13631710d9fa
kareemsasa3 <kareemsasa3@gmail.com>
2020-06-24 08:50:42 -0500

    set up program

---
+++ hangman.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+import random
+
+data_folder = Path("categories/")
+
+def initialize():
+    return 0
+
+def start_game():
+    return 0
+
+def choose_category():
+    return 0
+
+def play(file_name):
+    return 0
+    
+def update_path(file_name):
+    return 0
