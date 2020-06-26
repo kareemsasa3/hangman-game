919d2fc1e8ddec472e4f1eecae9573f8eb4aab67
kareemsasa3 <kareemsasa3@gmail.com>
2020-06-25 19:59:24 -0500

    created initialize function

--- hangman.py
+++ hangman.py
@@ -4,6 +4,24 @@ import random
 data_folder = Path("categories/")
 
 def initialize():
+    play_game = input("\nWould you like to play Hangman? (Y or N)\n")
+
+    if play_game in ('Y', 'y'):
+        start_game()
+    elif play_game in ('N', 'n'):
+        exit_game = input("\nWould you like to exit? (Y or N)\n")
+
+        if exit_game in ('Y', 'y'):
+            print("\nGoodbye\n")
+            exit
+        elif exit_game in ('N', 'n'):
+            initialize()
+        else:
+            print("\nUnknown user input\n")
+            initialize()
+    else:
+        print("\nUnknown user input\n")
+        initialize()
     return 0
 
 def start_game():
